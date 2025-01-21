a633bc6795706df395a5b6d973e12c364fcbcad1
Shahad <shahadalnaamani@outlook.com>
2025-01-21 13:50:16 +0400

    fixed error in logic (must be nullable to use .hasvalue)

--- Data/DTOs/TeamUpdateDTO.cs
+++ Data/DTOs/TeamUpdateDTO.cs
@@ -4,9 +4,9 @@
     {
         public int TeamID { get; set; }
         public string TeamName { get; set; }
-        public int TeamCapacity { get; set; }
+        public int? TeamCapacity { get; set; }
         public string Description { get; set; }
-        public decimal HourlyRate { get; set; }
+        public decimal? HourlyRate { get; set; }
         public decimal Rating { get; set; }
         public int CompletedProjects { get; set; }
         public bool Active { get; set; } = true;
